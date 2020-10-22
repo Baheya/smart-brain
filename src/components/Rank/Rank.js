62e9996d8ea7ca21e8b1da0af927229ebd70ffc8
Baheya Khalifa <baheya@riseup.co>
2020-10-22 15:30:36 +0400

    added ranking component that displays different emojis depending on number of faces detected

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Rank = ({ name, entries }) => {
+  const [emoji, setEmoji] = useState('');
+  useEffect(() => {
+    const generateEmoji = (entries) => {
+      fetch(`${process.env.REACT_APP_LAMBDA_API}${entries}`, {
+        method: 'GET',
+      })
+        .then((response) => response.json())
+        .then((data) => setEmoji(data.input))
+        .catch(console.log);
+    };
+    generateEmoji(entries);
+  }, [entries]);
   return (
     <div>
-      <div className='white f3'>
+      <div className="white f3">
         {`${name}, your current entry count is...`}
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
+      <div className="white f1">{entries}</div>
+      <div className="white f3">{`Rank badge: ${emoji}`}</div>
     </div>
   );
-}
+};
 
 export default Rank;
